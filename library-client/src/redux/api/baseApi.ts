d6a0042f22f15bee945bb43b0a56432f91e86310
Rakib Hossain <hossainrokib55@gmail.com>
2025-07-04 15:58:59 +0600

    env

--- library-client/src/redux/api/baseApi.ts
+++ library-client/src/redux/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const bookApi = createApi({
     reducerPath: 'bookApi',
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
     tagTypes: ["books"],
     endpoints: (build) => ({
         getBook: build.query({
--- library-client/src/redux/api/borrowApi.ts
+++ library-client/src/redux/api/borrowApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const borrowApi = createApi({
     reducerPath: 'borrowApi',
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
     tagTypes: ["borrow"],
     endpoints: (build) => ({
         getBorrowSummary: build.query({
